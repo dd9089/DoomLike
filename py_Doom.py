da23f0043b22833523e722c6d25a45e1b08d703f
Pacheco1197 <103137009+Pacheco1197@users.noreply.github.com>
2022-05-24 22:08:07 -0400

    Add files via upload

--- py_Doom.py
+++ py_Doom.py
@@ -4,9 +4,42 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 app = Ursina()
 window.fullscreen = True
 
+
+class Demon:
+    def __init__(self, health, speed, damage):
+        self.demon = Entity(model='sphere', color=color.white, texture = 'deamon', scale = 10, position = (2, 10, 20), collider = 'sphere')
+        self.health = health
+        self.speed = speed
+        self.damage = damage
+
+    def aggro(self):
+        if player.x > self.demon.x and distance(self.demon.position, player.position) > 2:
+            self.demon.x += .05
+        if player.x < self.demon.x and distance(self.demon.position, player.position) > 2:
+            self.demon.x -= .05
+        if player.z > self.demon.z and distance(self.demon.position, player.position) > 2:
+            self.demon.z += .05
+        if player.z < self.demon.z and distance(self.demon.position, player.position) > 2:
+            self.demon.z -= .05
+
+    def notice(self):
+        if distance(self.demon.position, player.position) >= 30:
+            self.aggro()
+
+    def attack(self):
+        if distance(self.demon.position, player.position) <= 1:
+            #player.setHealth(player.getHealth() - self.damage)
+            pass
+
+    def die(self):
+        destroy(self.demon)
+
+
 player = FirstPersonController()
 player.position = (0, 0, 0)
 player.scale = 1.5
+
+demon = Demon(10, 5, 2)
 
 ground = Entity (model = 'plane', color = color.red, texture = "grass", scale = 100, position = (0, 0, 0), collider = 'mesh')
 wall = [
@@ -24,11 +57,13 @@ def update():
         player.speed = 15
     else:
         player.speed = 10
-
-
-def on_click(self):
-    destroy(self)
-
+    if held_keys['e']:
+        demon.die()
+    try:
+        demon.notice()
+        demon.attack()
+    except:
+        pass
 
 
 
