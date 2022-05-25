21c0fbcdb0c3677962d3e9cf29727eb23b26c8c7
Pacheco1197 <103137009+Pacheco1197@users.noreply.github.com>
2022-05-25 11:08:37 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,74 +1,11 @@
 from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
-window.fullscreen = True
-window.exit_button.enabled = False
-window.borderless = True
-block = []
-
-player = Entity(model='sphere', color=color.white, texture="rainbow", scale=2)
-player2 = FirstPersonController()
-
-class Voxel(Button):
-    def __init__(self, position=(0,0,0)):
-        super().__init__(
-            parent = scene,
-            position = position,
-            model = 'cube',
-            origin_y = .5,
-            texture = 'white_cube',
-            color = color.color(0, 0, random.uniform(.9, 1.0)),
-            highlight_color = color.lime,
-        )
-
-
-def load_chunk(block, posX, posZ):
-    for z in range(8):
-        for x in range(8):
-            block.append(Voxel(position=(x + posX, 0, z + posZ)))
-
-
-load_chunk(block, 0, 0)
-load_chunk(block, -8, 0)
-load_chunk(block, 0, 8)
+cube = Entity (modle = 'cube', color = color.orange, scale = (2, 2, 2))
+cube.update()
+app.run()
 
 
 def update():
-    see_f = raycast(player2.position + (0, 0, 8), (0, 0, 8), distance=1)
-    see_l = raycast(player2.position + (-8, 0, 0), (-8, 0, 0), distance=1)
-    see_r = raycast(camera.world_position + (8, 0, 0), (8, 0, 0), distance=1)
-    see_b = raycast(player2.position + (0, 0, -8), (0, 0, -8), distance=1)
-
-    if not see_l.hit:
-        load_chunk(block, player2.x - 8, player2.z)
-    if not see_r.hit:
-        load_chunk(block, player2.x + 8, player2.z)
-    if not see_f.hit:
-        load_chunk(block, player2.x, player2.z + 8)
-    if not see_b.hit:
-        load_chunk(block, player2.x, player2.z - 8)
-
-    #print(player2.x, player2.z, "hello")
-
-    try:
-        player.rotation_y += held_keys['left arrow']
-        player.rotation_y -= held_keys['right arrow']
-        player.rotation_x += held_keys['down arrow']
-        player.rotation_x -= held_keys['up arrow']
-
-        player.y -= held_keys['k'] * .05
-        player.y += held_keys['i'] * .05
-        player.x -= held_keys['l'] * .05
-        player.x += held_keys['j'] * .05
-        player.z -= held_keys['u'] * .05
-        player.z += held_keys['o'] * .05
-    except:
-        pass
-
-    if held_keys['escape']:
-        quit()
-
-
-app.run()
+    cube.rotation_y += time.dt * 100
--- py_Doom.py
+++ py_Doom.py
@@ -8,7 +8,8 @@ Sky(texture='sky')
 
 class Demon:
     def __init__(self, health, speed, damage):
-        self.demon = Entity(model='sphere', color=color.white, texture = 'deamon', scale = 10, position = (2, 10, 20), collider = 'sphere')
+        self.demon = Entity(model='sphere', color=color.white, texture='deamon', scale=10, position=(2, 10, 20),
+                            collider='sphere')
         self.health = health
         self.speed = speed
         self.damage = damage
@@ -30,11 +31,44 @@ class Demon:
 
     def attack(self):
         if distance(self.demon.position, player.position) <= 1:
-            #player.setHealth(player.getHealth() - self.damage)
+            # player.setHealth(player.getHealth() - self.damage)
             pass
 
     def die(self):
         destroy(self.demon)
+
+
+class Slayer:
+    def __init__(self):
+        self.hp = 100
+        self.ammo = 0
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.shoot = True
+
+    def get_health(self):
+        return self.hp
+
+    def set_health(self, h):
+        self.hp = h
+        return self.hp
+
+    def get_ammo(self):
+        return self.ammo
+
+    def set_ammo(self, a):
+        self.ammo = a
+        return self.ammo
+
+    def shoot(self):
+        self.ammo = self.ammo - 1
+        '''if ():#crosshairs over demon):
+            demonHealth = demonHealth - 20
+        if (demonHealth = 0 or demonHealth < 0):
+            remove
+            demon
+            entity'''
 
 
 player = FirstPersonController()
@@ -44,13 +78,17 @@ player.jump_height = 4
 
 demon = Demon(10, 5, 2)
 
-ground = Entity (model = 'plane', color = color.red, texture = "grass", scale = 100, position = (0, 0, 0), collider = 'mesh')
+ground = Entity(model='plane', color=color.red, texture="grass", scale=100, position=(0, 0, 0), collider='mesh')
 wall = [
-    Entity(model='cube', color=color.white, texture='cobblestone', scale=(100, 20, 0.0005), position=(0, 5, 50), collider='mesh'),
-    Entity(model='cube', color=color.white, texture='cobblestone', scale=(100, 20, 0.0005), position=(0, 5, -50), collider='mesh'),
-    Entity(model='cube', color=color.white, texture='cobblestone', scale=(0.0005, 20, 100), position=(-50, 5, 0), collider='mesh'),
-    Entity(model='cube', color=color.white, texture='cobblestone', scale=(0.0005, 20, 100), position= (50, 5, 0), collider='mesh')
-    ]
+    Entity(model='cube', color=color.white, texture='cobblestone', scale=(100, 20, 0.0005), position=(0, 5, 50),
+           collider='mesh'),
+    Entity(model='cube', color=color.white, texture='cobblestone', scale=(100, 20, 0.0005), position=(0, 5, -50),
+           collider='mesh'),
+    Entity(model='cube', color=color.white, texture='cobblestone', scale=(0.0005, 20, 100), position=(-50, 5, 0),
+           collider='mesh'),
+    Entity(model='cube', color=color.white, texture='cobblestone', scale=(0.0005, 20, 100), position=(50, 5, 0),
+           collider='mesh')
+]
 
 
 def update():
@@ -67,6 +105,6 @@ def update():
         demon.attack()
     except:
         pass
-    
+
 
 app.run()
